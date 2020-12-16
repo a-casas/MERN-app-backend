ee67b7c0d6c6f9bbf5571803a4d1d871780d440b
Adrian Casas <adrian.casas.fernandez@gmail.com>
2020-12-16 09:03:38 +0000

    front url

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use((req, res, next) => {
 });
 app.use(cors({
   credentials: true,
-  origin: ["http://localhost:3001"]
+  origin: ["http://localhost:3001", "https://jappandgo.netlify.app"]
 }));
 app.use((req, res, next)=>{
   res.locals.user = req.user;
